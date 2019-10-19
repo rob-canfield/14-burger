92cf0eb34c9a4c0dd73e85f5aded3701a88bd7e9
Rob Canfield <git@livesenjoyable.com>
2019-10-19 00:20:50 -0600

    Routes and ORM finished. Getting errors upon running the server. Still need to create the html and handlebars.

--- config/orm.js
+++ config/orm.js
@@ -1,63 +1,36 @@
-var connection = require('./command');
-
-function printQuestionMarks(num) {
-  var arr = [];
-
-  for (var i = 0; i < num; i++) {
-    arr.push("?");
-  }
-
-  return arr.toString();
-}
-
-// Helper function to convert object key/value pairs to SQL syntax
-function objToSql(object) {
-  var arr = [];
-
-  // loop through the keys and push the key/value as a string int arr
-  for (var key in object) {
-    var value = object[key];
-    // check to skip hidden properties
-    if (Object.hasOwnProperty.call(object, key)) {
-      // if string with spaces, add quotations (Lana Del Grey => 'Lana Del Grey')
-      if (typeof value === "string" && value.indexOf(" ") >= 0) {
-        value = "'" + value + "'";
-      }
-      // e.g. {name: 'Lana Del Grey'} => ["name='Lana Del Grey'"]
-      // e.g. {sleepy: true} => ["sleepy=true"]
-      arr.push(key + "=" + value);
-    }
-  }d
-
-  // translate array of strings to a single comma-separated string
-  return arr.toString();
-}
+var connection = require('./connection');
 
 var orm = {
 
-    create: function(columns, values, callBack) {
-      var queryString = "INSERT INTO burgers (" + columns.toString() + ") VALUES (" + printQuestionMarks(values.length) + ")";
-  
-      connection.query(queryString, values, function(err, result) {
-        if (err) {
-          throw err;
-        }
-  
-        callBack(result);
-      });
-    },
-    // An example of objColVals would be {name: panther, sleepy: true}
-    update: function(objColVals, condition, callBack) {
-      var queryString = "UPDATE burger SET " + objToSql(objColVals) + " WHERE " + condition;
+  selectAll: function (callBack) {
+    var queryString = "SELECT * FROM bugrers;";
 
-      connection.query(queryString, function(err, result) {
-        if (err) {
-          throw err;
-        }
-  
-        callBack(result);
-      });
-    }
-  };
+    connection.query(queryString, values, function (err, result) {
+      if (err) { throw err; }
+
+      callBack(result);
+    });
+  },
+  // An example of objColVals would be {name: panther, sleepy: true}
+  insertOne: function (userInput, callBack) {
+    var queryString = "INSERT INTO burger (burger_name) VALUES (" + userInput + ")";
+
+    connection.query(queryString, function (err, result) {
+      if (err) { throw err; }
+
+      callBack(result);
+    });
+  },
+
+  updateOne: function (callBack) {
+    var queryString = "UPDATE burger SET devoured WHERE id =" 
+
+    connection.query(queryString, function (err, result) {
+      if (err) { throw err; }
+
+      callBack(result);
+    });
+  }
+};
 
   module.exports = orm;
--- models/burger.js
+++ models/burger.js
@@ -1,17 +1,21 @@
-// Import the ORM to create functions that will interact with the database.
 var orm = require("../config/orm.js");
 
 var burger = {
   all: function(callBack) {
-    orm.all("burgers", function(response) {
+    orm.selectAll("burgers", function(response) {
+      callBack(response);
+    });
+  },
+  create: function(cols, vals, callBack) {
+    orm.insertOne("cats", cols, vals, function(response) {
       callBack(response);
     });
   },
   update: function(objColVals, condition, callBack) {
-    orm.update("burgers", objColVals, condition, function(response) {
+    orm.updateOne("burgers", objColVals, condition, function(response) {
       callBack(response);
     });
   }
 }
-// Export the database functions for the controller (burgersController.js).
+
 module.exports = burger;
