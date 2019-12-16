af08c7c4645adfa4bbbddf4b38c1eb0f7da5a09d
Rob Canfield <git@livesenjoyable.com>
2019-12-15 23:10:18 -0700

    updated handlebars, having trouble getting db to display on page.

--- config/orm.js
+++ config/orm.js
@@ -22,8 +22,8 @@ var orm = {
     });
   },
   // An example of objColVals would be {name: panther, sleepy: true}
-  insertOne: function (userInput, callBack) {
-    var queryString = "INSERT INTO burger (burger_name) VALUES (" + userInput + ")";
+  insertOne: function (values, callBack) {
+    var queryString = "INSERT INTO burger (burger_name) VALUES (" + printQuestionMarks(values.length) + ")";
 
     connection.query(queryString, values, function (err, result) {
       if (err) { throw err; }
--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -37,7 +37,7 @@ router.put("/api/burgers/:id", function(req, res) {
     },
 
     condition,
-    
+
     function(result) {
       if (result.changedRows === 0) {
         // If no rows were changed, then the ID must not exist, so 404
