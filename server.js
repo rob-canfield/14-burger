a6732ac29c7afb62823f1eb9ace38febd2f78b8c
Rob Canfield <git@livesenjoyable.com>
2019-12-17 21:01:34 -0700

    styling and JS are now being loaded in the main page, thanks to help from Cody. Had to switch express routes to point to public folder. Stil need to get the GET POST and UPDATES working.

--- server.js
+++ server.js
@@ -1,11 +1,9 @@
-var express = require('express');
-
+var express = require("express");
+const path = require("path");
 var PORT = process.env.PORT || 8080;
-
 var app = express();
 
-app.use(express.static("public"));
-
+app.use("/public", express.static(path.join(__dirname, "public")));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
@@ -15,9 +13,8 @@ app.engine("handlebars", expressHandlebars({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
 var routes = require("./controllers/burgers_controller.js");
-
 app.use(routes);
 
-app.listen(PORT, function() {
+app.listen(PORT, function () {
   console.log("App now listening at localhost:" + PORT);
 });
